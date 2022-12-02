1b59daa99a38b910d05434b150b35569a05b6274
addehd <hej@adolfo.se>
2022-12-02 13:52:12 +0100

    Maximize or minimize sidebar with art

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,9 @@ import Image from 'next/image';
 export default function Navbar(){
  return (
   <div className="heading">
-    <Image src="/hangaren-solo.svg" alt="hangaren-solo" width={100} height={100} />
+    <a href='https://wordpress-863910-2986415.cloudwaysapps.com/'>
+      <Image src="/hangaren-solo.svg" alt="hangaren-solo" width={100} height={100} />
+    </a>
     {/* <h1>Hangaren</h1> */}
   </div>
  );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import Navbar from '../components/navbar.js';
 import { useState } from 'react';
 import Sequelize from "sequelize";
@@ -32,6 +30,11 @@ export async function getStaticProps() {
 
 export default function Home(props) {
   const [graffitisInMap, updateGraffity] = useState(props.art);
+  const [artSize, setArtSize] = useState(false);
+
+  const updateArtSize = () => {
+    setArtSize(!artSize);
+  }
   
   const updateGraffityInMap = (id) => {
     let newArray = graffitisInMap.map((graffiti) => {
@@ -49,10 +52,23 @@ export default function Home(props) {
     <>
     <Navbar />
 
-    <div className='art-photos'>
+    <div className={artSize ? "art-size-open" : "art-size-close"} style={{  position: "absolute", zIndex: "9" }} >
+      {/* <button className="art-size" onClick={()=>updateArtSize()}>artSize</button> */}
+      
+      {artSize
+        ?<div className="art-size" onClick={()=>updateArtSize()}>
+              <img src="/mini.svg" alt="" />
+         </div>
+        :<div className="art-size" onClick={()=>updateArtSize()}>
+              <img src="/maxi.svg" alt="" />
+         </div>
+      }
+
+
       {graffitisInMap.map((graffiti) => (
       <div id={graffiti.submission_id} className='art' key={graffiti.submission_id}>
-        <Image layout="responsive" alt={graffiti.name}  width={100} height={100} src={graffiti.img}   />
+        {/* <Image layout="responsive" alt={graffiti.name} className="test"  width={100} height={100} src={graffiti.img}   /> */}
+        <img src={graffiti.img} alt={graffiti.name} className="test" style={{ width: "100%"}}/>
         {/* <h1 className={ JSON.parse(graffiti.active) ? "red" : "blue"}> {graffiti.name}</h1> */}
       </div>
       ))}
@@ -60,13 +76,13 @@ export default function Home(props) {
 
     <Map className="map" mapLib={maplibregl} 
       initialViewState={{ longitude: 12.98, latitude: 55.580, zoom: 14, pitch: 85, pinch: 200, bearing: 0, }}
-      style={{width: "100%", top: "4.5rem", background: "rgb(255, 64, 0)", position: "fixed", height: "95vh"}}
+      style={{width: "100%", top: "6rem", background: "rgb(255, 64, 0)", position: "fixed", height: "95vh"}}
       mapStyle="https://api.maptiler.com/maps/streets/style.json?key=InUWzr8s1xkknwxF8ZG8">
 
       {graffitisInMap.map((graffiti) => (
         <Marker key={graffiti.submission_id} longitude={graffiti.position.split(',')[1]} latitude={graffiti.position.split(',')[0]}>
-          {/* <img className="" onClick={()=>updateGraffityInMap(graffiti.submission_id)} width={80} src="/pin-explosions.svg" alt="" /> */}
-          <Image onClick={()=>updateGraffityInMap(graffiti.submission_id)} src="/pin-explosions.svg" alt="" width={80} height={80}   />
+          <img className="" onClick={()=>updateGraffityInMap(graffiti.submission_id)} width={80} src="/pin-explosions.svg" alt="" />
+          {/* <Image onClick={()=>updateGraffityInMap(graffiti.submission_id)} src="/pin-explosions.svg" alt="" width={80} height={80}   /> */}
         </Marker>
       ))}
       <NavigationControl />
