419d0e9458765dc403ec034b56e8675f7ee8acce
addehd <hej@adolfo.se>
2022-12-08 12:52:03 +0100

    show title, photografer and geotips field on urbanart

--- db/db.js
+++ db/db.js
@@ -4,21 +4,37 @@ SELECT DISTINCT
  name.value as name,
  img.value as img,
  submission.submission_id as submission_id,
- active.value as active
+ active.value as active,
+ artist_name.value as artist,
+ photograf.value as photograf,
+ artinfo.value as artinfo,
+ geotip.value as geotip
 FROM 
  wp_e_submissions_values as position,
  wp_e_submissions_values as name,
  wp_e_submissions_values as img,
  wp_e_submissions_values as submission,
- wp_e_submissions_values as active
+ wp_e_submissions_values as artist_name,
+ wp_e_submissions_values as photograf,
+ wp_e_submissions_values as active,
+ wp_e_submissions_values as artinfo,
+ wp_e_submissions_values as geotip
 WHERE
- position.key  = "position" and
+ position.key = "position" and
  name.key = "name" and
+ artist_name.key = "konstnar" and
+ photograf.key = "fotograf" and
  img.key = "field_417c055" and 
  active.key = "active" and
+ artinfo.key = "artInfo" and
+ geotip.key = "geotips" and
+ artinfo.submission_id = submission.submission_id and
+ geotip.submission_id = submission.submission_id and
+ photograf.submission_id = submission.submission_id and
  img.submission_id = submission.submission_id and
  position.submission_id = submission.submission_id and
- name.submission_id = submission.submission_id and 
+ name.submission_id = submission.submission_id and
+ artist_name.submission_id = submission.submission_id and
  active.submission_id = submission.submission_id
 ORDER BY submission_id DESC
 LIMIT 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Sequelize from "sequelize";
 import dbConfig from "../db/db.config.js";
 import { sqlQuery } from "../db/db.js";
+import Pin from '../components/pin.js';
 
 import Map, {NavigationControl, Marker} from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
@@ -31,30 +32,34 @@ export async function getStaticProps() {
 export default function Home(props) {
   const [graffitisInMap, updateGraffity] = useState(props.art);
   const [artSize, setArtSize] = useState(false);
+  const [artInfo, setArtInfo] = useState(false);
 
   const updateArtSize = () => {
     setArtSize(!artSize);
   }
-  
+
   const updateGraffityInMap = (id) => {
     let newArray = graffitisInMap.map((graffiti) => {
       if (graffiti.submission_id === id) { graffiti.active = true; }
       return graffiti; });
     updateGraffity(newArray);
-    scrollToArt(id);
-  }
+    scrollToArt(id); }
 
   function scrollToArt (id) {
     const el = document.getElementById(id);
-    el.scrollIntoView({ behavior: 'instant', block: "center", inline: "center"});} 
+    el.scrollIntoView({ behavior: 'instant', block: "center", inline: "center"}); } 
 
+  const updateArtInfo = (e) => {
+    console.log(this)
+    setArtInfo(!artInfo);
+    console.log(e)
+  }
+    
   return (
     <>
     <Navbar />
-
-    <div className={artSize ? "art-size-open" : "art-size-close"} style={{  position: "absolute", zIndex: "9" }} >
-      {/* <button className="art-size" onClick={()=>updateArtSize()}>artSize</button> */}
-      
+    
+    <div className={artSize ? "art-size-open test" : "art-size-close test"} style={{  position: "absolute", zIndex: "9" }} >
       {artSize
         ?<div className="art-size" onClick={()=>updateArtSize()}>
           <img src="/mini.svg" alt="" />
@@ -64,10 +69,17 @@ export default function Home(props) {
          </div>
       }
 
-
       {graffitisInMap.map((graffiti) => (
       <div id={graffiti.submission_id} className='art' key={graffiti.submission_id}>
         {/* <Image layout="responsive" alt={graffiti.name} className="test"  width={100} height={100} src={graffiti.img}   /> */}
+       
+        <div style={{display: artInfo ? 'flex':'flex'}} class="art-info">
+          { graffiti.name ? <p><span></span>   { graffiti.name } </p> : <p></p> }
+          {/* { graffiti.photograf ? <p><span style={{ paddingLeft: "0.5rem"}}>Foto:</span> { graffiti.photograf } </p> : <p></p> } */}
+        </div>
+        
+        {/* <img src='/info.svg' className="show-info" onClick={()=>updateArtInfo(graffiti.submission_id)}/> */}
+         
         <img src={graffiti.img} alt={graffiti.name} className="test" style={{ width: "100%"}}/>
         {/* <h1 className={ JSON.parse(graffiti.active) ? "red" : "blue"}> {graffiti.name}</h1> */}
       </div>
